d5db0e0ca39a177bbbca69b7eecbc86331faa79f
Edelwiess <wangxin86@gmail.com>
2018-02-10 19:52:32 +0800

    Create text.py

---
+++ text.py
@@ -0,0 +1,8 @@
+print ('Python was started in 1989 by \"Guido\".')
+print (r'"Hamlet"')
+print ('\"To be, or not to be\": that is the question.\nWhether it\'s nobler in the mind to suffer.')
+print ('===========================================================================================')
+print ('''"To be, or not to be": that is the question.
+Whether it's nobler in the mind to suffer.''')
+
+input()
