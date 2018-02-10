8d556db142c4d0cef27def51c17160c8e4d95e28
Edelwiess <wangxin86@gmail.com>
2018-02-10 19:19:08 +0800

    Create While.py

---
+++ While.py
@@ -0,0 +1,21 @@
+x1 = 1
+d = 3
+t = 1
+s = x1
+print ('t =', t)
+print ('x1=', x1)
+print ('s =', s)
+print ('================')
+
+while t < 100:
+  x1 = x1+d
+  t = t+1
+  s = s+x1
+  print ('t =', t)
+  print ('x1 =', x1)
+  print ('s =', s)
+  print ('================')
+
+
+
+input()
