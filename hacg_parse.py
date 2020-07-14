f2653ad707e8ae593bc6207716a293c8f7f6b44b
Edelwiess <wangxin86@gmail.com>
2020-07-14 14:31:51 +0800

    Update hacg_parse.py

--- hacg_parse.py
+++ hacg_parse.py
@@ -12,6 +12,9 @@ hacgurl = 'https://www.liuli.se/wp/79053.html'
 req = urllib.request.Request(hacgurl, headers=headers)
 response = urllib.request.urlopen(req)
 response = response.read().decode('utf8')
+title = re.search(r'<title>(.*?)</title>', response)
+if title:
+    title = title[1]
 response = remove_tags_with_content(response, which_ones=('script','style'))
 response = remove_tags(response)
 #print(response)
@@ -19,7 +22,7 @@ hash_result = re.search(r'[a-fA-F0-9]{40,40}', response, re.M)
 magnet_result = ''
 if hash_result:
     magnet_result='magnet:?xt=urn:btih:'+hash_result[0]
-
+print(title)
 print(magnet_result)
 
 
